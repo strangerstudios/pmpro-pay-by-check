f24da37aaae378b1513b4defee314252c983dfed
Jason Coleman <jason@strangerstudios.com>
2017-08-04 12:24:49 -0400

    updating add on documentation url

--- pmpro-pay-by-check.php
+++ pmpro-pay-by-check.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Paid Memberships Pro - Pay by Check Add On
-Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-pay-by-check/
+Plugin URI: https://www.paidmembershipspro.com/add-ons/pmpro-pay-by-check-add-on/
 Description: A collection of customizations useful when allowing users to pay by check for Paid Memberships Pro levels.
 Version: .7.8
 Author: Stranger Studios
@@ -975,7 +975,7 @@ function pmpropbc_plugin_row_meta($links, $file) {
 	if(strpos($file, 'pmpro-pay-by-check.php') !== false)
 	{
 		$new_links = array(
-			'<a href="' . esc_url('http://www.paidmembershipspro.com/add-ons/plugins-on-github/pmpro-pay-by-check-add-on/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url('https://www.paidmembershipspro.com/add-ons/pmpro-pay-by-check-add-on/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
 			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
 		);
 		$links = array_merge($links, $new_links);
