9c6fc6aaa6ff8e0ac9368e7af9661e9d6d2cf46a
Jarryd Long <JarrydLong@users.noreply.github.com>
2023-08-28 15:24:28 +0200

    Rather show the fields when the Address for Free Levels is active

--- pmpro-pay-by-check.php
+++ pmpro-pay-by-check.php
@@ -312,12 +312,12 @@ function pmpropbc_init_include_billing_address_fields()
     			
 		if($options['setting'] == 2)
 		{
-			//hide billing address and payment info fields
-			add_filter('pmpro_include_billing_address_fields', '__return_false', 20);
-			add_filter('pmpro_include_payment_information_fields', '__return_false', 20);
-
-			//Make sure the billing address fields aren't still required when using the Address for Free Levels Add On
-			remove_action("pmpro_required_billing_fields", "pmproaffl_pmpro_required_billing_fields", 30);
+			//Only hide the address if we're not using the Address for Free Levels Add On
+			if( ! function_exists( 'pmproaffl_pmpro_required_billing_fields' ) ) {				
+				//hide billing address and payment info fields
+				add_filter('pmpro_include_billing_address_fields', '__return_false', 20);
+				add_filter('pmpro_include_payment_information_fields', '__return_false', 20);
+			}
 
 			//Hide the toggle section if the PayPal Express Add On is active
 			remove_action( "pmpro_checkout_boxes", "pmproappe_pmpro_checkout_boxes", 20 );
